9fb38667c0f03789aee85f33d7118079fdbf7ed4
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-02-19 15:16:03 -0600

    saving recent progress

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+var healthPoints
+var attackPower
+var counterAttack
+
+
+};
+// var player
+// var enemies
+// var hp
+// var attack
+// var lose
+// var win
+
+// function attack increase
+
