26aa4f94639bb2cb16fe13dec15d1fe93100d79c
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-02-23 23:05:18 -0600

    added a click function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,115 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
-var healthPoints
-var attackPower
-var counterAttack
+
+    var player1 = {
+    hpCounter:120,
+    attackPower:35,
+    attack:function(otherPlayer) {
+      otherPlayer.hp - this.attackPower
+    }
+
+    };
 
 
-};
+
+    var player2 = {
+      hpCounter:150,
+      attackPower:45,
+      attack:function(otherPlayer) {
+        otherPlayer.hp - this.attackPower
+    }
+    
+    };
+
+
+    var player3 = {
+      hpCounter:250,
+      attackPower:80,
+      attack:function(otherPlayer) {
+        otherPlayer.hp - this.attackPower
+    }
+    
+    };
+
+
+    var player4 = {
+      hpCounter:200,
+      attackPower:100,
+      attack:function(otherPlayer) {
+        otherPlayer.hp - this.attackPower
+    }
+    
+    };
+
+    var players = [player1, player2,player3, player4];
+
+    console.log(player2.hpCounter);
+
+    var counterAttack = {
+      attackPower:
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+  $("#lukeSkywalker").on("click","a,img", function (e) {
+      e.preventDefault();
+      alert('You Clicked Me');
+
+  })
+
+
+
+
+  // function startGame{
+
+
+
+
+  // }
+
+
+
+  };
+
+});
+
+
+
+
+
+
+
+  
+
+
+// function moveDefender{}
+
+
+// function roundComplete() {}
+
+
+// button.on(click(function();
+
+// });
+
+
+
+
+// startGame();
+
+
+
 // var player
 // var enemies
 // var hp
