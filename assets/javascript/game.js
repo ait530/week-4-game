395f326ee8ebbd922a9777cb255eb9e27a303e50
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-02-24 21:54:00 -0600

    about to implement pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,99 +1,134 @@
-$(document).ready(function() {
+/*Code below will run once the DOM is ready*/
+$(document).ready(function(){
+  /*console.log( 'ready!' );*/
 
 
+  /*Objects with all of each player's attributes*/
+  var player1 = { 
+  hpCounter:120,
+  attackPower:35,
+  counterAttack:50,
+  attack:function(otherPlayer) {
+    otherPlayer.hp - this.attackPower
+  }
+  attackIncrease:function(otherPlayer) {
+    otherPlayer.hp - this.attackPower[1] ++;
+  }
 
-    var player1("#lukeSkywalker") = { 
-    hpCounter:120,
-    attackPower:35,
-    counterAttack:50,
+  };
+
+  
+  var player2 = {
+    hpCounter:150,
+    attackPower:45,
+    counterAttack:54,
     attack:function(otherPlayer) {
       otherPlayer.hp - this.attackPower
-    }
+  }
 
-    
+  };
+
+
+  var player3 = {
+    hpCounter:250,
+    attackPower:80,
+    counterAttack:57,
+    attack:function(otherPlayer) {
+      otherPlayer.hp - this.attackPower
+  }
+  
+  };
+
+
+  var player4 = {
+    hpCounter:200,
+    attackPower:100,
+    counterAttack:51,
+    attack:function(otherPlayer) {
+      otherPlayer.hp - this.attackPower
+  }
+
+  };
+
+  /*Storing */
+  $("#lukeSkywalker").data("player1");  
+  $("#obiwan").data("player2");
+  $("#darthvader").data("player3");
+  $("#darthsidious").data("player4");
+
+  
+  /*An array variable that refers to the players collectively*/
+  var players = ["player1", "player2", "player3", "player4"];
+
+ 
+  /*Sets game up, moves all character to the character selection area.*/
+  /*function startGame(){
+
+    $("players").append(".characterSelection");
+
+    if ($("player").on("click","a,img", function (e) {
+        e.preventDefault(); 
+
+        $("player").append("enemiesAttack");
 
     };
-
-
-
-    var player2 = {
-      hpCounter:150,
-      attackPower:45,
-      attack:function(otherPlayer) {
-        otherPlayer.hp - this.attackPower
-    }
-    
-    };
-
-
-    var player3 = {
-      hpCounter:250,
-      attackPower:80,
-      attack:function(otherPlayer) {
-        otherPlayer.hp - this.attackPower
-    }
-    
-    };
-
-
-    var player4 = {
-      hpCounter:200,
-      attackPower:100,
-      attack:function(otherPlayer) {
-        otherPlayer.hp - this.attackPower
-    }
-    
-    };
-
-    var players = [player1, player2,player3, player4];
-
-    console.log(player2.hpCounter);
-
-    var counterAttack = {
-      attackPower:
-
-
-
-    
-
-
-
-
-
-
+  })*/
 
 
 
 
   $("#lukeSkywalker").on("click","a,img", function (e) {
-      e.preventDefault();
-      alert('You Clicked Me');
+        e.preventDefault();
+        console.log('You Clicked Me');
+      });
 
-  })
+    $("#obiwan").on("click","a,img", function (e) {
+        e.preventDefault();
+        console.log('You Clicked Me');
+      });
+
+    $("#darthVader").on("click","a,img", function (e) {
+        e.preventDefault();
+        console.log('You Clicked Me');
+      });
+
+    $("#darthSidious").on("click","a,img", function (e) {
+        e.preventDefault();
+        console.log('You Clicked Me');
+      });
 
 
 
+
+
+
+});
+  // function enemies() {
+
+  //   if 
+
+
+  // }
+
+
+  
+  // var counterAttack = {
+  //   attackPower:
 
   // function startGame{
 
 
 
 
-  // }
+  // // }
 
-
-
-  };
-
-});
+  // };
 
 
 
 
 
 
-
-  
 
 
 // function moveDefender{}
